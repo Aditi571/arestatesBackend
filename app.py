71c4e0b3f5025c37b52fad592a97f7c605425cdd
Aditi Rawat <aditirawat@Aditis-MacBook-Air.local>
2024-12-20 19:04:33 +0530

    port changed

--- app.py
+++ app.py
@@ -191,4 +191,5 @@ def get_user_details(current_user):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
